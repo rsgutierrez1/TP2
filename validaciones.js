e17cf2ab1f49075286d395efb984b9b896ea5704
rsgutierrez1 <129602246+rsgutierrez1@users.noreply.github.com>
2023-06-09 18:00:15 -0300

    Add files via upload

---
+++ validaciones.js
@@ -0,0 +1,22 @@
+var nombre = document getElementById('nombre');
+var telefono = document getElementById('telefono');
+var email = document getElementById('email');
+var sueldo = document getElementById('sueldo');
+function enviarFormulario() {
+    console.log ('Enviando formulario');
+    return false;
+    
+}
+
+
+
+
+
+const formulario = document.getElementById('formulario');
+const inputs = document.querySelectorAll
+
+const expresiones = {
+    nombre:/^[a-zA-Z0-9\]{5,30}$/,
+    empresa: /^[a-zA-Z0-9\]{5,30}$/,
+    telefono: /^{}
+}
